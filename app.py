5fd6640a33cfdbd4114167df6be6c0a655b26ca5
Cheryl Braus <76953517+cherylab@users.noreply.github.com>
2022-07-03 09:25:46 -0400

    state

--- app.py
+++ app.py
@@ -316,6 +316,8 @@ def blindPage():
 
     st.write("<br><br>", unsafe_allow_html=True)
     if st.button(label="Show Company Mapping"):
+        st.write(st.session_state.compOrderDict)
+        st.write(sorted(st.session_state.compOrderDict.items()))
         for (k,v) in dict(sorted(st.session_state.compOrderDict.items())):
             st.write(f"{k}: {v}")
         # st.write(compOrderDict)
