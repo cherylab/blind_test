cf71e8fa43236cec69ec83a37b1982c5f24f5e1f
Cheryl Braus <76953517+cherylab@users.noreply.github.com>
2022-07-05 18:11:08 -0400

    state print

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def blindPage():
     if "order" not in st.session_state:
         st.session_state['order'] = order
 
-    # st.write("session state", st.session_state)
+    st.write("session state", st.session_state)
 
     testdf = df[df.Symbol.isin(options)]
 
@@ -269,22 +269,6 @@ def blindPage():
                             min_value=min(order),
                             max_value=max(order),
                             value=1)
-
-    # # the callback function for the button will add 1 to the
-    # # slider value up to 10
-    # def plus_one():
-    #     if st.session_state["slider"] < 10:
-    #         st.session_state.slider += 1
-    #     else:
-    #         pass
-    #     return
-    #
-    # # when creating the button, assign the name of your callback
-    # # function to the on_click parameter
-    # add_one = st.button("Add one to the slider", on_click=plus_one, key="add_one")
-    #
-    # # create the slider
-    # slide_val = st.slider("Pick a number", 0, 10, key="slider")
 
     def addGoodComp():
         if st.session_state.goodbox == "":
